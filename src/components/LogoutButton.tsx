aa1bb6d6d61729ed1b38c02c33b8a69999b7d95d
valentina vittoria <valeviit@icloud.com>
2023-03-13 16:35:08 +0100

    create http hooks , auth0 auth & create user in db

---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+    return (
+      <>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+            </button>
+        </>
+  );
+};
+
+export default LogoutButton;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+
+
+
+
+const VITE_DOMAIN = import.meta.env.VITE_DOMAIN;
+const VITE_CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App />
+    <Auth0Provider
+        domain={VITE_DOMAIN}
+        clientId={VITE_CLIENT_ID}
+        authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+        <App />
+    </Auth0Provider>
 )
