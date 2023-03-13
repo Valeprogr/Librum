4a4f48aee2c2f9980522c5e38233b27a0e4e01f6
valentina vittoria <valeviit@icloud.com>
2023-03-13 17:09:52 +0100

    edit sidebar login logout btn & function edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { AboutPage, BooksPage, EditProfilePage, HomePage, LocationPage, LoginPage, SignUpPage } from './pages';
 import { BrowserRouter, json, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
 import Profile from './pages/Profile';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Spinner } from './components/spinner/Spinner';
@@ -13,21 +11,6 @@ import { useHttp } from './hooks/http.hook';
 
 function App() {
   const { isAuthenticated, isLoading, user } = useAuth0();
-  const [data, setData] = useState();
-  const { request } = useHttp();
-
-  const url = 'http://localhost:9090/user/create';
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      if (user) {
-        const data =  request(url, 'POST',user.email);
-      }
-    }
-  }, [])
-
-
-
   if (isLoading) {
     return <Spinner />
   }
@@ -39,8 +22,6 @@ function App() {
           <BrowserRouter>
             <div>
               <Navbar />
-              <LogoutButton />
-              {/* <button onClick={getData}>get user</button> */}
               <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='/about' element={<AboutPage />} />
@@ -58,7 +39,6 @@ function App() {
           <BrowserRouter>
             <div>
               <Navbar />
-              <LoginButton />
               <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='/about' element={<AboutPage />} />
