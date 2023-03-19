7815f157012120eb2e5443d954aedeb0c8f97352
valentina vittoria <valeviit@icloud.com>
2023-03-20 00:52:14 +0100

    create cart component and edit profile

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -5,7 +5,8 @@ interface CTAButton {
     clickEvent?: () => void
 }
 
-const CTAButton = ({cssProps, children,clickEvent}: CTAButton) => {
+const CTAButton = ({ cssProps, children, clickEvent }: CTAButton) => {
+
   return (
     <button onClick={clickEvent} className={`rounded-lg font-bold text-card p-2 px-4 bg-dark border-none shadow-md hover:bg-hover ${cssProps}`}>{children}</button>
   )
