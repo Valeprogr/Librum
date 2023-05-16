a1554e5debef683f69fa840afc960e979821f538
valentina vittoria <valeviit@icloud.com>
2023-05-16 17:50:54 +0200

    create carousel componet, searchbar & edit Home componet

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,20 @@
+import Carousel from "../components/Carousel";
+import Searchbar from "../components/spinner/Searchbar";
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="h-auto w-auto px-[24px] md:px-20">
+      <div className="flex flex-col lg:flex-row lg:justify-between container mx-10 lg:mx-auto py-3 border-b border-gray-300">
+        <Searchbar />
+        <div className="flex">
+          <div className="w-24 px-2 font-semibold cursor-pointer">CUPON30%</div>
+          <div className="w-24 px-2 font-semibold text-red-600">SALE</div>
+        </div>  
+      </div>
+
+      <div className="2xl:container 2xl:mx-auto 2xl:px-0 py-3 px-10">
+        <Carousel />
+      </div>
+    </div>
   )
 }
 
