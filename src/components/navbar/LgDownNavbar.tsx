20c5c504fa7dcdc655cfe932908bfb7e34cf2e63
valentina vittoria <valeviit@icloud.com>
2023-05-16 17:50:54 +0200

    create carousel componet, searchbar & edit Home componet

--- src/components/navbar/LgDownNavbar.tsx
+++ src/components/navbar/LgDownNavbar.tsx
@@ -4,7 +4,7 @@ const LgDownNavbar = () => {
   return (
     <div className='md:flex items-center justify-start hidden  bg-card w-full shadow-lg border-t-gray-500/20 border-t-[1px] '>
     <div className="flex justify-start gap-4  mx-auto   max-w-[1300px] items-center px-20 py-1 w-full">
-    <Link to='/home' className="text-base font-[500] px-3   cursor-pointer p-[4px]  hover:bg-dark/10 rounded-lg transition-all duration-300">Home</Link>
+    <Link to='/' className="text-base font-[500] px-3   cursor-pointer p-[4px]  hover:bg-dark/10 rounded-lg transition-all duration-300">Home</Link>
     <Link to='/books' className="text-base font-[500] px-3   cursor-pointer p-[4px]  hover:bg-dark/10 rounded-lg transition-all duration-300">Books</Link>
     <Link to='/location' className="text-base font-[500] px-3   cursor-pointer p-[4px]  hover:bg-dark/10 rounded-lg transition-all duration-300">Location</Link>
     <Link to='/about' className="text-base font-[500] px-3   cursor-pointer p-[4px]  hover:bg-dark/10 rounded-lg transition-all duration-300">About</Link>
