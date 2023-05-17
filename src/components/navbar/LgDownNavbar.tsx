cdfd95505245f0fc5602ecf09fcfb4f59db149df
Luka Brkovic <coding@lbrkovic.com>
2023-05-17 23:44:37 +0200

    dropdown z-index, navbar shadow

--- src/components/navbar/LgDownNavbar.tsx
+++ src/components/navbar/LgDownNavbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const LgDownNavbar = () => {
   return (
-    <div className='md:flex items-center justify-start hidden  bg-card w-full shadow-lg border-t-gray-500/20 border-t-[1px] '>
+    <div className='md:flex items-center justify-start hidden  bg-card w-full shadow-sm border-t-gray-500/20 border-t-[1px] '>
     <div className="flex justify-start gap-4  mx-auto   max-w-[1300px] items-center px-20 py-1 w-full">
     <Link to='/' className="text-base font-[500] px-3   cursor-pointer p-[4px]  hover:bg-dark/10 rounded-lg transition-all duration-300">Home</Link>
     <Link to='/books' className="text-base font-[500] px-3   cursor-pointer p-[4px]  hover:bg-dark/10 rounded-lg transition-all duration-300">Books</Link>
