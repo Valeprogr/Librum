55f3cf01803fa0271d5ba51fd2f7efd7afe5139d
valentina vittoria <valeviit@icloud.com>
2023-05-24 18:16:26 +0200

    edit book card

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -10,16 +10,14 @@ interface IBookCard {
 const BookCard = ({ clickEvent, props }: IBookCard, key: number) => {
   const { getItemQuantity, increaseCartQuantity} = useShoppingCart();
   const quantity = getItemQuantity(props._id)
-  const clickHandler = () => {
-    console.log(props._id)
-  }
+
   return (
     <div key={key} className='w-[290px] flex flex-col justify-start py-2 pt-8 items-center px-4'>
       <img src={props.imageUrl} alt='book' className='w-[290px] rounded-lg bg-card p-5 ' />
       <div className='pt-3'>
         <h2 className='text-[20px] font-[500]'>{props.title}</h2>
         <h3 className="text-[18px] font-[400] ">{props.author}</h3>
-        <p className='text-[15px] text-dark/80'>{props.description}</p>
+        <p className='text-[15px] text-dark/80 line-clamp-4'>{props.description}</p>
         <div className="flex justify-between items-center pt-2">
           <button className="text-[14px] rounded-lg font-bold text-card p-2 px-4 bg-dark border-none shadow-md hover:bg-hover" onClick={() => increaseCartQuantity(props._id, props.author,props.title, props.price,props.imageUrl)}>Add to Cart</button>
           <button onClick={clickEvent} className="hover:bg-dark/10 rounded-lg transition-all duration-300 p-1.5 px-4 text-[16px] font-bold">Read more... </button>
