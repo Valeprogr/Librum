d321c3d252a98816b77980b2bc09a9d1b746be8a
valentina vittoria <valeviit@icloud.com>
2023-06-12 23:34:49 +0200

    final touch

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ const HomePage = () => {
     <div className="h-auto w-auto px-[24px] md:px-20 ">
       <div className="flex flex-col lg:flex-row lg:justify-between container lg:mx-10 py-3 border-b border-gray-300">
         <Searchbar />
-        <div className="flex">
+        <div className="flex mt-2">
           <div className="w-24 px-2 font-semibold cursor-pointer">CUPON30%</div>
           <div className="w-24 px-2 font-semibold text-red-600 cursor-pointer">SALE</div>
         </div>  
