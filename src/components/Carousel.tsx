c38aec7cdb44d4c20a02fc10476062487696c78b
valentina vittoria <valeviit@icloud.com>
2023-07-18 23:49:45 +0200

    edit a tag for the carousel images

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -171,7 +171,7 @@ const maxScrollWidth = useRef(0);
               className="carousel-item text-center relative w-64 h-[440px] snap-start"
             >
               <a
-                href=""
+                href={`/book/${resource.id}`}
                 className="h-full w-full aspect-square block bg-origin-padding bg-left-top bg-cover bg-no-repeat z-0"
                 style={{ backgroundImage: `url(${resource.imageUrl || ''})` }}
               >
@@ -182,7 +182,7 @@ const maxScrollWidth = useRef(0);
                 />
               </a>
               <a
-                href=""
+                href={`/book/${resource.id}`}
                 className="h-full w-full aspect-square block absolute top-0 left-0 transition-opacity duration-300 opacity-0 hover:opacity-100 bg-blue-800/75 z-10"
               >
                 <h3 className="text-white py-6 px-3 mx-auto text-xl">
